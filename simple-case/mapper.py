919acd9e5997cf7f128ffaeea08579d3fcc66dc4
Rizqi Jawwadin <114014406+rizqijawwadin@users.noreply.github.com>
2024-06-18 22:30:25 +0700

    basic code is only for initial testing

--- simple-case/mapper.py
+++ simple-case/mapper.py
@@ -2,18 +2,18 @@
 """mapper.py"""
 
 import sys
+import re
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    # replace non-letters with spaces
+    line = re.sub(r'[^a-zA-Z\s]', ' ', line)
     # split the line into words
     words = line.split()
     # increase counters
     for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
+        # write the results to STDOUT (standard output)
         # tab-delimited; the trivial word count is 1
-        print('%s\t%s' % (word, 1))
+        print('%s\t%s' % (word.lower(), 1))
